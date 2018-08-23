c5000b02d58d88515824335854a9edce56fc4b2f
Curtis <sharkydart@gmail.com>
2018-08-23 18:25:06 -0400

    trying to get intents to fire from widget, causing it to refresh data in listview

--- app/src/main/java/com/example/curtis/bakingapp/widget/BakingWidget.java
+++ app/src/main/java/com/example/curtis/bakingapp/widget/BakingWidget.java
@@ -1,10 +1,12 @@
 package com.example.curtis.bakingapp.widget;
 
+import android.app.IntentService;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.example.curtis.bakingapp.R;
@@ -16,17 +18,22 @@ import com.example.curtis.bakingapp.RecipeListActivity;
  */
 public class BakingWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget);
-        views.setRemoteAdapter(R.id.widget_list, new Intent(context, WidgetService.class));
-        //TODO: load the recipe list as the list
-       //TODO: load the ingredient list when a recipe is selected
+
+        Intent raIntent = new Intent(context, WidgetService.class);
+        views.setRemoteAdapter(R.id.widget_list, raIntent);
+
+        //TODO: load the ingredient list when a recipe is selected
         //TODO: allow to navigate back to the recipe list
-        Intent intent = new Intent(context, RecipeListActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        Intent refreshIntent = new Intent(context, WidgetDataProvider.class);
+        PendingIntent pendingRefreshIntent = PendingIntent.getBroadcast(context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.widget_list, pendingRefreshIntent);
+
+        Intent appIntent = new Intent(context, RecipeListActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, appIntent, 0);
         views.setOnClickPendingIntent(R.id.widget_img_launcher, pendingIntent);
 
         // Instruct the widget manager to update the widget
@@ -37,8 +44,15 @@ public class BakingWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
+            Log.d("fart", "updating a widget!");
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("fart", "received something!");
+        super.onReceive(context, intent);
     }
 
     @Override
--- app/src/main/java/com/example/curtis/bakingapp/widget/WidgetDataProvider.java
+++ app/src/main/java/com/example/curtis/bakingapp/widget/WidgetDataProvider.java
@@ -2,37 +2,98 @@ package com.example.curtis.bakingapp.widget;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+import android.widget.Toast;
+
+import com.example.curtis.bakingapp.R;
+import com.example.curtis.bakingapp.RecipeDetailFragment;
+import com.example.curtis.bakingapp.RecipeListActivity;
+import com.example.curtis.bakingapp.model.Recipe;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
-    List<String> collection = new ArrayList<>();
-    Context context;
-    Intent intent;
 
-    private void initData(){
-        collection.clear();
-        for(int i=0;i <=10;i++){
-            collection.add("ListView Item" + i);
-        }
-    }
-    public WidgetDataProvider(Context context, Intent intent) {
-        this.context = context;
-        this.intent = intent;
-    }
+    public static final String THE_INGREDIENTS = "the ingredients for the recipe";
+    private ArrayList<Recipe> collection = new ArrayList<>();
+    private Context theContext;
+    private Intent theIntent;
 
     @Override
     public void onCreate() {
         initData();
     }
-
     @Override
     public void onDataSetChanged() {
         initData();
+    }
+
+    private void initData(){
+        collection.clear();
+        if(theIntent.getAction() != null && theIntent.getAction().equals(THE_INGREDIENTS))
+            Log.d("fart", "action: " + THE_INGREDIENTS + "!!!");
+
+        Bundle theBundle = theIntent.getBundleExtra(THE_INGREDIENTS);
+        if(theBundle != null)
+            if(theBundle.containsKey(THE_INGREDIENTS))
+                Toast.makeText(theContext, "Ingredients!", Toast.LENGTH_LONG).show();
+            else
+                Log.d("fart", "no key");
+
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(theContext);
+        String theRecipes = sharedPref.getString(RecipeListActivity.BAKING_JSON, "A recipe doesn't have a name.");
+//        Log.d("fart", theRecipes);
+
+        if(theRecipes.length() > 0) {
+            try {
+                JSONArray theRecipesJSON = new JSONArray(theRecipes);
+                for (int i = 0; i < theRecipesJSON.length(); i++) {
+                    JSONObject currentObj = theRecipesJSON.getJSONObject(i);
+                    Recipe tempRecipe = new Recipe(currentObj);
+                    collection.add(tempRecipe);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(theContext, "Recipe Problem", Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+            Toast.makeText(theContext, "Please load the app first, Miriam", Toast.LENGTH_LONG).show();
+    }
+    public WidgetDataProvider(Context context, Intent intent) {
+        this.theContext = context;
+        this.theIntent = intent;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews remoteView = new RemoteViews(theContext.getPackageName(), android.R.layout.simple_list_item_1);
+
+        remoteView.setTextViewText(android.R.id.text1, collection.get(position).getTheName());
+        remoteView.setTextColor(android.R.id.text1, Color.BLACK);
+
+        //adds information unique to the view item at the position into a bundle, which is put in the intent for the list item
+        // ...defining the unique action
+        Bundle extras = new Bundle();
+        extras.putParcelableArrayList(THE_INGREDIENTS, collection.get(position).getTheIngredients());
+        Intent fillInIntent = new Intent();
+        fillInIntent.setAction(THE_INGREDIENTS);
+        fillInIntent.putExtra(THE_INGREDIENTS, extras);
+        remoteView.setOnClickFillInIntent(android.R.layout.simple_list_item_1, fillInIntent);
+
+        return remoteView;
     }
 
     @Override
@@ -43,15 +104,6 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
     @Override
     public int getCount() {
         return collection.size();
-    }
-
-    @Override
-    public RemoteViews getViewAt(int position) {
-        RemoteViews remoteView = new RemoteViews(context.getPackageName(),
-                android.R.layout.simple_list_item_1);
-        remoteView.setTextViewText(android.R.id.text1, collection.get(position));
-        remoteView.setTextColor(android.R.id.text1, Color.BLACK);
-        return remoteView;
     }
 
     @Override
