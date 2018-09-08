7176503a52488eef2a0559bd85cb228025a8d642
curtis <sharkydart@gmail.com>
2018-09-08 12:50:48 -0400

    Espresso test implemented!
    ...let's see what they still want

---
+++ app/src/androidTest/java/com/example/curtis/bakingapp/AdapterTest.java
@@ -0,0 +1,37 @@
+package com.example.curtis.bakingapp;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.instanceOf;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class AdapterTest {
+    @Rule
+    public ActivityTestRule<RecipeListActivity> mActivityTestRule = new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Test
+    public void clickRecyclerViewItem_OpensRecipeDetail(){
+//        onData(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withId(R.id.rvStep_list)).check(matches(isDisplayed()));
+    }
+}
